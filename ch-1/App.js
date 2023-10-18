7f5e7eb2d4c2c40fe809bb962725d56bda52ed8b
muhammad2 <asifbsit@gmail.com>
2023-10-18 22:31:29 +0500

    add graphql schema object

--- ch-1/App.js
+++ ch-1/App.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
+
 app.use(bodyParser.json());
 app.get("/", (req, res, next) => {
   res.send("Graphql backend server started");
 });
 
+app.use(
+  "graphql",
+  graphqlHttp({
+    schema: null,
+    rootValue: {},
+  })
+);
 app.listen(3000);
