493ba426614f9a752e83e1103b2d080a8f04a8f7
muhammad2 <asifbsit@gmail.com>
2023-10-25 20:37:55 +0500

    added graphqp schema in backend with static data

--- ch-1/App.js
+++ ch-1/App.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const graphqlHttp = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
 const app = express();
@@ -9,12 +9,56 @@ app.use(bodyParser.json());
 app.get("/", (req, res, next) => {
   res.send("Graphql backend server started");
 });
-
+const events = [];
 app.use(
-  "graphql",
-  graphqlHttp({
-    schema: null,
-    rootValue: {},
+  "/graphql",
+
+  graphqlHTTP({
+    schema: buildSchema(`
+    type Event {
+      _id:ID!
+      title:String!
+      description:String!
+      price:Float!
+      date:String!
+    }
+    input EventInput {
+      title:String!
+      description:String!
+      price:Float!
+      date:String!
+    }
+    type RootQuery {
+      events:[Event!]!
+    }
+
+    type RootMutation {
+      createEvent(inputEvent:EventInput) : Event
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+
+    }
+          `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.inputEvent.title,
+          price: +args.inputEvent.price,
+          description: args.inputEvent.description,
+          date: args.inputEvent.date,
+        };
+        events.push(event);
+        return event;
+      },
+    },
+    graphiql: true,
   })
 );
 app.listen(3000);
